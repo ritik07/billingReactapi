9d67112b9dd6e3d3871455b14ceca8d9fcea83a6
ritik-s-07 <ritik.soni@reliablesoft.co.in>
2021-02-03 16:49:28 +0530

    CRUD API BILLING

---
+++ API/Controller/Ravana/getRavana.js
@@ -0,0 +1,15 @@
+const { getRavana } = require("../../Query/Ravana/getRavanQuery");
+
+module.exports = {
+  getRavanaFunction: (req, res) => {
+    getRavana(req.params, (iResponseCode, sResponse, result) => {
+      console.log("result", result);
+      res.status(200).json({
+        success: iResponseCode === 100 ? true : false,
+        messagecode: iResponseCode,
+        message: sResponse,
+        data: result,
+      });
+    });
+  },
+};
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const RavanaRoute = require("./API/Routes/Ravana/routes");
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api", RavanaRoute);
+
+app.listen(7000, () => {
+  console.log("server up and running");
+});
