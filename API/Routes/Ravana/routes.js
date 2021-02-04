4bfa79b80881c2e3bf0732a380b5be9429e754d1
ritik-s-07 <ritik.soni@reliablesoft.co.in>
2021-02-04 21:44:50 +0530

    test

--- API/Routes/Ravana/routes.js
+++ API/Routes/Ravana/routes.js
@@ -1,23 +1,20 @@
-// const express = require("express");
-// const route = express.Router();
+const express = require("express");
+const route = express.Router();
 
-route.get("/getravana", (req, res) => {
-  res.send("aaya");
-});
-// const { getRavanaFunction } = require("../../Controller/Ravana/getRavana");
-// const {
-//   insertRavanaFunction,
-// } = require("../../Controller/Ravana/insertRavana");
-// const {
-//   updateRavanaFunction,
-// } = require("../../Controller/Ravana/updateRavana");
-// const { deleteRavanaFuction } = require("../../Controller/Ravana/deleteRavana");
+const { getRavanaFunction } = require("../../Controller/Ravana/getRavana");
+const {
+  insertRavanaFunction,
+} = require("../../Controller/Ravana/insertRavana");
+const {
+  updateRavanaFunction,
+} = require("../../Controller/Ravana/updateRavana");
+const { deleteRavanaFuction } = require("../../Controller/Ravana/deleteRavana");
 
-// route.get("/getravana/", getRavanaFunction);
-// route.get("/getravana/:iRavanaID", getRavanaFunction);
+route.get("/getravana/", getRavanaFunction);
+route.get("/getravana/:iRavanaID", getRavanaFunction);
 
-// route.post("/insertravana", insertRavanaFunction);
-// route.post("/updateravana", updateRavanaFunction);
-// route.post("/deleteravana", deleteRavanaFuction);
+route.post("/insertravana", insertRavanaFunction);
+route.post("/updateravana", updateRavanaFunction);
+route.post("/deleteravana", deleteRavanaFuction);
 
-// module.exports = route;
+module.exports = route;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const RavanaRoute = require("./api/Routes/Ravana/routes");
+const ravanaroutes = require("./api/Routes/Ravana/routes");
 
 app.use(cors());
 app.use(express.json());
@@ -10,7 +10,7 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
-app.use("/api", RavanaRoute);
+app.use("/api", ravanaroutes);
 
 app.listen(process.env.PORT || 7000, () => {
   console.log("server up and running");
