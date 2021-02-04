10739677d305c824f1072b14bfbadd857f5a25f9
ritik-s-07 <ritik.soni@reliablesoft.co.in>
2021-02-04 19:16:40 +0530

    test

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const RavanaRoute = require("./api/Routes/Ravana/routes");
+// const RavanaRoute = require("./api/Routes/Ravana/routes");
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api", RavanaRoute);
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+// app.use("/api", RavanaRoute);
 
 app.listen(7000, () => {
   console.log("server up and running");
