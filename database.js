3769e13879818e9a6cb3817408187a9bc4738881
Ritik Soni <34769340+ritik07@users.noreply.github.com>
2022-09-15 23:59:23 +0530

    P

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const { createPool } = require("mysql");
 const pool = createPool({
   // port: 3306,
   host: "148.66.136.10",
-  user: "TEST_ROGER",
+  user: "TESTROGER",
   password: "roger1234",
   database: "ROGER_DB",
   connectionLimit: 150,
